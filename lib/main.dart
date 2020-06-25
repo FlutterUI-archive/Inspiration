77856b0aa3c37591ff20beea5a5eda7af3be958b
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-25 22:17:21 +0530

    Modified the search field

--- lib/main.dart
+++ lib/main.dart
@@ -26,24 +26,35 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             width: double.infinity,
-            decoration: BoxDecoration(color: Colors.white),
+            decoration: BoxDecoration(color: Colors.white,
+            ),
             padding: EdgeInsets.all(20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Find Your",
                     style: TextStyle(color: Colors.black87, fontSize: 25)),
-                SizedBox(height: 5,),
-              Text("Inspiration",style:TextStyle(color: Colors.black,fontSize: 40)),
-              Container(
-                child:TextField(
-                  decoration:InputDecoration()
-                )
-              )
+                SizedBox(
+                  height: 5,
+                ),
+                Text("Inspiration",
+                    style: TextStyle(color: Colors.black, fontSize: 40)),
+                SizedBox(height: 8,),
+                Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(227, 232, 232, 1),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: TextField(decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.search,color: Colors.black87,),
+                      hintText: "What are you looking for?",
+                      hintStyle: TextStyle(color: Colors.grey,fontSize:16)
+                    )))
               ],
             ),
           )
