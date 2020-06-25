fff565fe193627109cc6258c179b693d3e4b1ad5
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-25 23:52:39 +0530

    Completed the entire UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
+  runApp(HomePage());
 }
 
 class HomePage extends StatefulWidget {
@@ -15,116 +13,122 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        elevation: 0,
-        leading: IconButton(
-            icon: Icon(Icons.menu, color: Colors.black87), onPressed: () {}),
-      ),
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(30),
-                )),
-            padding: EdgeInsets.all(20.0),
+    return MaterialApp(
+      //theme: ThemeData(textTheme: GoogleFonts.robotoCondensedTextTheme()),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          elevation: 0,
+          leading: IconButton(
+              icon: Icon(Icons.menu, color: Colors.black87), onPressed: () {}),
+        ),
+        body: SafeArea(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Find Your",
-                    style: TextStyle(color: Colors.black87, fontSize: 25)),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Inspiration",
-                    style: TextStyle(color: Colors.black, fontSize: 40)),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(227, 232, 232, 1),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Colors.black87,
-                            ),
-                            hintText: "What are you looking for?",
-                            hintStyle:
-                                TextStyle(color: Colors.grey, fontSize: 16)))),
-                SizedBox(height: 10),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Today's Picks",
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                SizedBox(height: 15),
-                Container(
-                    height: 200,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        promoCard("assets/images/one.jpg"),
-                        promoCard("assets/images/two.jpg"),
-                        promoCard("assets/images/three.jpg"),
-                        promoCard("assets/images/four.jpg")
-                      ],
-                    )),
-                SizedBox(height: 20),
-                Container(
-                  height: 140,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/images/four.jpg")),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(30),
+                  )),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Find Your",
+                      style: TextStyle(color: Colors.black87, fontSize: 25)),
+                  SizedBox(
+                    height: 5,
                   ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(
-                            begin: Alignment.bottomRight,
-                            stops: [
-                              0.2,
-                              0.9
-                            ],
-                            colors: [
-                              Colors.black.withOpacity(.8),
-                              Colors.black.withOpacity(0)
-                            ])),
-                            child: Align(
-                              alignment: Alignment.bottomLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(14.0),
-                                child: Text("Have a nice day!", style: TextStyle(color:Colors.white,fontSize: 20)),
+                  Text("Inspiration",
+                      style: TextStyle(color: Colors.black, fontSize: 40,fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(227, 232, 232, 1),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.black87,
                               ),
-                            ),
-                  ),
-                ),
-              ],
+                              hintText: "What are you looking for?",
+                              hintStyle: TextStyle(
+                                  color: Colors.grey, fontSize: 16)))),
+                  SizedBox(height: 10),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Today's Picks",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 15),
+                  Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          promoCard("assets/images/one.jpg"),
+                          promoCard("assets/images/two.jpg"),
+                          promoCard("assets/images/three.jpg"),
+                          promoCard("assets/images/four.jpg")
+                        ],
+                      )),
+                  SizedBox(height: 20),
+                  Container(
+                    height: 140,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/four.jpg")),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [
+                                0.2,
+                                0.9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(0)
+                              ])),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Text("Have a nice day!",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )),
+      ),
     );
   }
 
